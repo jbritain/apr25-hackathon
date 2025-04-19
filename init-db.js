79d4cfc8d71401ea5b13cc458ffbec757b9975aa
maxha <max/hannah4@outlook.com>
2025-04-19 14:48:58 +0100

    id fix

--- init-db.js
+++ init-db.js
@@ -3,7 +3,7 @@ function initDB(db){
       db.run(`
           CREATE TABLE IF NOT EXISTS user (
               name TEXT NOT NULL,
-              userID TEXT NOT NULL PRIMARY KEY,
+              userID INTEGER NOT NULL PRIMARY KEY,
               isTeacher BOOLEAN NOT NULL,
               password TEXT NOT NULL
           )`
@@ -18,7 +18,7 @@ function initDB(db){
 
         db.run(`
             CREATE TABLE IF NOT EXISTS user_class (
-                userID TEXT NOT NULL,
+                userID INTEGER NOT NULL,
                 classCode INTEGER NOT NULL,
                 PRIMARY KEY (userID, classCode),
                 FOREIGN KEY (userID) REFERENCES user(userID),
@@ -28,7 +28,7 @@ function initDB(db){
         
         db.run(`
             CREATE TABLE IF NOT EXISTS exercise (
-                exerciseID TEXT NOT NULL PRIMARY KEY,
+                exerciseID INTEGER NOT NULL PRIMARY KEY,
                 name TEXT NOT NULL,
                 incentive TEXT NOT NULL,
                 pointValue INTEGER NOT NULL,
@@ -39,23 +39,24 @@ function initDB(db){
 
         db.run(`
             CREATE TABLE IF NOT EXISTS question (
-                questionID TEXT NOT NULL PRIMARY KEY,
+                questionID INTEGER NOT NULL PRIMARY KEY,
                 questionNumber INTEGER NOT NULL,
                 questionPicture BLOB NOT NULL,
                 questionText TEXT NOT NULL,
-                exerciseID TEXT NOT NULL,
+                isCorrect BOOLEAN NOT NULL,
+                exerciseID INTEGER NOT NULL,
                 FOREIGN KEY (exerciseID) REFERENCES exercise(exerciseID)
             )`
           )
 
         db.run(`
             CREATE TABLE IF NOT EXISTS answer (
-                answerID TEXT NOT NULL PRIMARY KEY,
+                answerID INTEGER NOT NULL PRIMARY KEY,
                 answerImage BLOB NOT NULL,
                 answerText TEXT NOT NULL,
                 isCorrect BOOLEAN NOT NULL,
-                userID TEXT NOT NULL,
-                questionID TEXT NOT NULL,
+                userID INTEGER NOT NULL,
+                questionID INTEGER NOT NULL,
                 FOREIGN KEY (userID) REFERENCES user(userID),
                 FOREIGN KEY (questionID) REFERENCES question(questionID)
             )`
@@ -63,11 +64,11 @@ function initDB(db){
 
         db.run(`
             CREATE TABLE IF NOT EXISTS message (
-                messageID TEXT NOT NULL PRIMARY KEY,
+                messageID INTEGER NOT NULL PRIMARY KEY,
                 content TEXT NOT NULL,
                 dateTime DATETIME NOT NULL,
-                userID TEXT NOT NULL,
-                questionID TEXT NOT NULL,
+                userID INTEGER NOT NULL,
+                questionID INTEGER NOT NULL,
                 FOREIGN KEY (userID) REFERENCES user(userID),
                 FOREIGN KEY (questionID) REFERENCES question(questionID)
             )`, (err) => {
